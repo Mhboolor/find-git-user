2ba4a859aab559b873d5c3d3ba9dfdcd6de1c016
Mhboolor <mhstare81@example.com>
2023-07-25 14:42:29 +0330

    feat:add error message for search bos

--- src/components/search/SearchUser.jsx
+++ src/components/search/SearchUser.jsx
@@ -3,13 +3,15 @@ import { useNavigate } from "react-router-dom";
 
 function SearchUser() {
   const [search, setSearch] = useState('');
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [error , setError] = useState();
 
   const searchHandler = () => {
     if(search.length >= 3){
       navigate(`/${search}`)
+      setError('')
     }else{
-      return
+      setError("حداقل باید 3 کاراکتر وارد کنید !!!")
     }
   }
 
@@ -19,14 +21,17 @@ function SearchUser() {
         <button className=" bg-green-400 px-2 py-1 rounded-md font-medium" onClick={searchHandler}>
           جستجو
         </button>
-        <input
-          type="text"
-          placeholder="جستجوی کاربر ..."
-          className="border rounded-md px-2 py-1 outline-0 focus-visible:border-black"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
+          <input
+            type="text"
+            placeholder="جستجوی کاربر ..."
+            className="border rounded-md px-2 py-1 outline-0 focus-visible:border-black"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
       </div>
+      {
+        error && <p className="text-center text-red-500">{error}</p>
+      }
       <div className="flex items-center justify-center">
         <p className="text-lg">کاربری را جستجو کنید</p>
       </div>
