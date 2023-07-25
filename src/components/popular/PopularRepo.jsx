86d0dc31384d9c204d5cc2021cf111c7148cf2a2
Mhboolor <mhstare81@example.com>
2023-07-25 23:28:51 +0330

    feat:add sorting logic for repoes

--- src/components/popular/PopularRepo.jsx
+++ src/components/popular/PopularRepo.jsx
@@ -8,6 +8,7 @@ function PopularRepo() {
   const [loading, setLoading] = useState(true);
   const [repo, setRepo] = useState([]);
   const [filtered, setFiltered] = useState([]);
+  const [sort, setSort] = useState();
 
   useEffect(() => {
     const fetchRepo = async () => {
@@ -27,14 +28,36 @@ function PopularRepo() {
     fetchRepo();
   }, []);
 
-  const filterRepo = (param) => {
-    if(param.length >= 3){
-      const filteredRepo = repo.filter(repo => repo.name.toLowerCase().includes(param.toLowerCase()))
-      setFiltered(filteredRepo)
-    }else{
-      setFiltered([])
+  useEffect(() => {
+    let result;
+    switch (sort) {
+      case "most-star":
+        result = [...repo].sort((prev , next) => next.stargazers_count - prev.stargazers_count)
+        setFiltered(result)
+        break;
+      case "most-fork":
+        result = [...repo].sort((prev , next) => next.forks - prev.forks)
+        setFiltered(result)
+        break;
+      case "last-update":
+        result = [...repo].sort((prev , next) => new Date(next.updated_at) - new Date(prev.updated_at))
+        setFiltered(result)
+        break;
+      default:
+        break;
     }
-  }
+  }, [sort]);
+
+  const filterRepo = (param) => {
+    if (param.length >= 3) {
+      const filteredRepo = repo.filter((repo) =>
+        repo.name.toLowerCase().includes(param.toLowerCase())
+      );
+      setFiltered(filteredRepo);
+    } else {
+      setFiltered([]);
+    }
+  };
 
   return (
     <div className="bg-white rounded-sm p-5">
@@ -50,7 +73,11 @@ function PopularRepo() {
           <div className="font-medium flex items-center justify-between flex-col sm:flex-row gap-4">
             <h3 className="sm:text-xl ">10 ریپازیتوری با بیشترین ستاره</h3>
             <div className="flex gap-4 flex-col sm:flex-row">
-              <select>
+              <select
+                className="border rounded-md outline-0 p-2"
+                value={sort}
+                onChange={(e) => setSort(e.target.value)}
+              >
                 <option value="most-star">بیشترین ستاره</option>
                 <option value="most-fork">بیشترین فورک</option>
                 <option value="last-update">اخرین آپدیت</option>
@@ -59,7 +86,7 @@ function PopularRepo() {
                 type="text"
                 placeholder="جستجو کنید ..."
                 className=" bg-slate-100 border px-2 py-1 rounded-sm outline-0 focus-visible:border-black"
-                onChange={e => filterRepo(e.target.value)}
+                onChange={(e) => filterRepo(e.target.value)}
               />
             </div>
           </div>
@@ -74,37 +101,35 @@ function PopularRepo() {
                   <th className="border p-2">تاریخ آپدیت</th>
                 </tr>
               </thead>
-              {
-                filtered.length > 0 ? (
-                  <tbody>
-                    {
-                      filtered.map(item => (
-                        <tr className="text-center" key={item.id}>
-                          <td className="p-2 border ">{item.name}</td>
-                          <td className="p-2 border "><Link>{item.svn_url}</Link></td>
-                          <td className="p-2 border ">{item.stargazers_count}</td>
-                          <td className="p-2 border ">{item.forks}</td>
-                          <td className="p-2 border ">{item.updated_at}</td>
-                        </tr>
-                      ))
-                    }
-                  </tbody>
-                ) : (
-                  <tbody>
-                    {
-                      repo.map(item => (
-                        <tr className="text-center" key={item.id}>
-                          <td className="p-2 border ">{item.name}</td>
-                          <td className="p-2 border "><Link>{item.svn_url}</Link></td>
-                          <td className="p-2 border ">{item.stargazers_count}</td>
-                          <td className="p-2 border ">{item.forks}</td>
-                          <td className="p-2 border ">{item.updated_at}</td>
-                        </tr>
-                      ))
-                    }
-                  </tbody>
-                )
-              }
+              {filtered.length > 0 ? (
+                <tbody>
+                  {filtered.map((item) => (
+                    <tr className="text-center" key={item.id}>
+                      <td className="p-2 border ">{item.name}</td>
+                      <td className="p-2 border ">
+                        <Link>{item.svn_url}</Link>
+                      </td>
+                      <td className="p-2 border ">{item.stargazers_count}</td>
+                      <td className="p-2 border ">{item.forks}</td>
+                      <td className="p-2 border ">{item.updated_at}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              ) : (
+                <tbody>
+                  {repo.map((item) => (
+                    <tr className="text-center" key={item.id}>
+                      <td className="p-2 border ">{item.name}</td>
+                      <td className="p-2 border ">
+                        <Link>{item.svn_url}</Link>
+                      </td>
+                      <td className="p-2 border ">{item.stargazers_count}</td>
+                      <td className="p-2 border ">{item.forks}</td>
+                      <td className="p-2 border ">{item.updated_at}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              )}
             </table>
           </div>
         </div>
